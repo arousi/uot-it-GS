26bd23b88a37ac17fbc6865a6963970dae455801
vsc-sanad <41429866+arousi@users.noreply.github.com>
2025-05-10 19:19:23 +0200

    [init] ITGS211 intro to OOP w/ java

---
+++ ITGS211/NewClass.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author AAROU
+ */
+public class NewClass {
+
+  String type = "Gel";
+  String Color = "Blue";
+  int point = 10;
+  static boolean clicked = false;
+  
+  public static void click(){
+      clicked = true;
+  }
+  public static void unclick(){
+      clicked = false;
+  }
+}
---
+++ ITGS211/newJavaFile5.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+public class NewClass {
+
+  String type = "Gel";
+  String Color = "Blue";
+  int point = 10;
+  static boolean clicked = false;
+  
+  public static void click(){
+      clicked = true;
+  }
+  public static void unclick(){
+      clicked = false;
+  }
+}
+
+
+
+
+
+
+
+/**
+ *
+ * @author AAROU
+ */
+public class newJavaFile5 {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        
+        NewClass p1 = new NewClass();
+        // TODO code application logic here
+    }
+}
